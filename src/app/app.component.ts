6c300aca8ad1fd1d63f40df9a928d6f204aff4ba
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-01-06 16:59:48 +0100

    Update OnInit according service refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MessageType } from './core/interfaces/message-interface';
 import { WsChatService } from './core/services/ws-chat.service';
 
 @Component({
@@ -10,7 +11,17 @@ export class AppComponent implements OnInit, OnDestroy {
   public constructor(private chatService: WsChatService) {}
 
   ngOnInit(): void {
-    this.chatService.doConnect();
+    this.chatService.doConnect().then(() => {
+      // Let's present to server
+      this.chatService.send(
+        'chat.register',
+        {
+          sender: 'dacodemaniak',
+          type: MessageType.JOIN
+        }
+      );
+    });
+
   }
   ngOnDestroy(): void {
     this.chatService.doDisconnect();
